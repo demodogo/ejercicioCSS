962e859ec5e5139677d1652afecb26360e48bcf6
demodogos <macaigm@gmail.com>
2021-12-26 22:52:41 -0300

    Speed change JS

--- JS/proyecto.js
+++ JS/proyecto.js
@@ -1,18 +1,18 @@
  $(".signInBtn").click(showUp);
 
 function showUp() {
-     $(".cuadrado-signin").css( { transition: "transform 1s", transform:  "translateX(0%)" } );
-     $(".inac-signin").css({transition: "transform 1s", transform: "translateX(-100%)"});
-     $(".cuadrado-signup").css({transition: "transform 1s", transform: "translateX(100%)"});
-     $(".inac-signup").css({transition: "transform 1s", transform: "translateX(0%)"});
+     $(".cuadrado-signin").css( { transition: "transform 0.7s", transform:  "translateX(0%)" } );
+     $(".inac-signin").css({transition: "transform 0.7s", transform: "translateX(-100%)"});
+     $(".cuadrado-signup").css({transition: "transform 0.7s", transform: "translateX(100%)"});
+     $(".inac-signup").css({transition: "transform 0.7s", transform: "translateX(0%)"});
  }
 
  $(".signUpBtn").click(showIn);
 
  function showIn() {
-      $(".cuadrado-signup").css( { transition: "transform 1s", transform:  "translateX(0%)" } );
-      $(".inac-signup").css({transition: "transform 1s", transform: "translateX(100%)"});
-      $(".cuadrado-signin").css({transition: "transform 1s", transform: "translateX(-100%)"});
-      $(".inac-signin").css({transition: "transform 1s", transform: "translateX(0%)"});
+      $(".cuadrado-signup").css( { transition: "transform 0.7s", transform:  "translateX(0%)" } );
+      $(".inac-signup").css({transition: "transform 0.7s", transform: "translateX(100%)"});
+      $(".cuadrado-signin").css({transition: "transform 0.7s", transform: "translateX(-100%)"});
+      $(".inac-signin").css({transition: "transform 0.7s", transform: "translateX(0%)"});
   }
  
